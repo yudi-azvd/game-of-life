1048a23a48a6e91fc70f1de0b4ce59f571b80044
yudi-azvd <yudiyamane@gmail.com>
2019-11-23 21:31:39 -0300

    add .css

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const land = [[]]
-const landWidth = 5
-const landHeight = 2
+const land = []
+const landWidth = 10
+const landHeight = 10
 
 start()
 
 function start() {
 	initializeLand()
+	renderLand()
 }
 
 function initializeLand() {
 	for (let row = 0; row < landHeight; row++) {
-		for (let column = 0; column < landWidth; column++)
+		land[row] = new Array(landWidth)
+
+		for (let column = 0; column < landWidth; column++) {
 			land[row][column] = 0
+		}
 	}
+}
+
+function renderLand() {
+	var html = ''
+
+	html += '<table cellpadding=0 cellspacing=0>'
+
+	for(let row = 0; row < landHeight; row++) {
+		html += '<tr>'
+
+		for(let column = 0; column < landWidth; column++) {
+			html += '<td>'
+			html += row*landWidth+column
+			html += '</td>'
+		}		
+
+		html += '</tr>'
+	}
+
+	html += '</table>'
+
+	document.querySelector('#land-canvas').innerHTML = html
 }
 
