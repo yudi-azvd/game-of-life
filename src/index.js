8cce9bdfb6680de6295ebc988bb26b82b5778a4c
yudi-azvd <yudiyamane@gmail.com>
2019-11-23 23:20:00 -0300

    toggle cell content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const land = []
-const landWidth = 10
-const landHeight = 10
+const landWidth = 8
+const landHeight = 8
+const debugMode = false
+
+
 
 start()
 
 function start() {
 	initializeLand()
-	renderLand()
+	setInterval(renderLand, 500)
+	// renderLand()
 }
 
 function initializeLand() {
@@ -14,7 +18,7 @@ function initializeLand() {
 		land[row] = new Array(landWidth)
 
 		for (let column = 0; column < landWidth; column++) {
-			land[row][column] = 0
+			land[row][column] = 0 //false
 		}
 	}
 }
@@ -28,9 +32,19 @@ function renderLand() {
 		html += '<tr>'
 
 		for(let column = 0; column < landWidth; column++) {
-			html += '<td>'
-			html += row*landWidth+column
-			html += '</td>'
+			const cellIndex = row*landWidth + column
+
+			if (debugMode) {
+				html += `<td id=${cellIndex}>`
+				html += `<div class="cell-index">${cellIndex}</div>`
+				html += land[row][column] //? 1:0
+				html += '</td>'
+			}
+			else {
+				html += `<td id=${cellIndex} class="${land[row][column] ? "alive" : "dead" }">`
+				html += `<div class="cell-index hidden">${cellIndex}</div>`
+				html += '</td>'
+			}
 		}		
 
 		html += '</tr>'
@@ -39,5 +53,22 @@ function renderLand() {
 	html += '</table>'
 
 	document.querySelector('#land-canvas').innerHTML = html
+	addEventListenerToCells()
+}
+
+function addEventListenerToCells() {
+	const cells = document.querySelectorAll('td')
+	for(let i = 0; i < cells.length; i++) {
+		cells[i].addEventListener('click', toggleCellContent)
+	}
+}
+
+function toggleCellContent(e) {
+	const cell = e.target
+	const cellIndex = parseInt(cell.getAttribute('id'))
+	const column = cellIndex % landWidth
+	const row = parseInt(cellIndex/landWidth)
+
+	land[row][column] = land[row][column] ? 0 : 1
 }
 
